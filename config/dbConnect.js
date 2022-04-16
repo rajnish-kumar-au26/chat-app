9cfbe8431f4eb01abbe1cd4bb3058f143c3f57d1
shubhankar <shubhankarroy974@gmail.com>
2022-04-16 14:28:38 +0530

    update file structure

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
-function dbConnect() {
-  mongoose
-    .connect("mongodb://localhost:27017/chat-app")
-    .then(() => console.log(`DB is Connected`))
-    .catch((err) => console.log(`DB is Not Connect`, err));
-}
-
-module.exports = dbConnect;
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log(`DB is Connected`))
+  .catch((err) => console.log(`DB is Not Connect`, err));
